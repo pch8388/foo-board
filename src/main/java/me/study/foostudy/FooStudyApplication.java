a4ecfcc2822e9bcf153c354f6298e29f9f40bf85
SC <pch838811@gmail.com>
2021-02-24 23:19:19 +0900

    feat: init

---
+++ src/main/java/me/study/foostudy/FooStudyApplication.java
@@ -0,0 +1,13 @@
+package me.study.foostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/study/foostudy/FooStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.foostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
