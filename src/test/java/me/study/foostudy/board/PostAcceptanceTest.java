6a4958254bd797faa34832bf68183fc8584c977b
SC <pch838811@gmail.com>
2021-07-03 10:03:34 +0900

    refactor : 인수테스트 리팩터링
    - 테스트 문서화 부분 메서드로 분리
    - 테스트 가독성을 위해 private 메서드들 위치 조정

--- src/test/java/me/study/foostudy/board/PostAcceptanceTest.java
+++ src/test/java/me/study/foostudy/board/PostAcceptanceTest.java
@@ -30,6 +30,48 @@ public class PostAcceptanceTest extends AcceptanceTest {
 		게시글_등록_되어있음("새로운 게시글 제목", "새로운 게시글 내용을 등록합니다.");
 	}
 
+	@DisplayName("게시글 목록을 조회한다")
+	@Test
+	void searchPosts() {
+		// given, when  게시글을 3개 등록한다
+		게시글_등록_되어있음("새로운 게시글 제목 - 1", "새로운 게시글 내용을 등록합니다. - 1");
+		게시글_등록_되어있음("새로운 게시글 제목 - 2", "새로운 게시글 내용을 등록합니다. - 2");
+		게시글_등록_되어있음("새로운 게시글 제목 - 3", "새로운 게시글 내용을 등록합니다. - 3");
+
+		// then  게시글 목록을 조회하여 3개가 맞는지 확인한다
+		게시글_목록_조회(3);
+	}
+
+	private void 게시글_목록_조회(int size) {
+		final List<ResponsePostDto> responsePosts = client.get().uri("/posts")
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBodyList(ResponsePostDto.class)
+			.consumeWith(getDocument("post-list-item", getPostListResponseSnippet()))
+			.returnResult()
+			.getResponseBody();
+
+		assertThat(responsePosts).isNotNull();
+		assertThat(responsePosts.size()).isEqualTo(size);
+	}
+
+	@DisplayName("게시글을 삭제한다")
+	@Test
+	void deletePost() {
+		// TODO : 2021/06/23 게시글을 등록한다   -ksc
+		// TODO : 2021/06/23 등록된 게시글을 삭제한다   -ksc
+		// TODO : 2021/06/23 삭제되었는지 확인한다   -ksc
+	}
+
+	@DisplayName("게시글을 수정한다")
+	@Test
+	void updatePost() {
+		// TODO : 2021/06/23 게시글을 등록한다   -ksc
+		// TODO : 2021/06/23 등록된 게시글을 수정한다   -ksc
+		// TODO : 2021/06/23 수정되었는지 확인한다   -ksc
+	}
+
 	private void 게시글_등록_되어있음(String title, String content) {
 		// given, when
 		final ResponsePostDto responseDto = client.post().uri("/posts")
@@ -75,52 +117,14 @@ public class PostAcceptanceTest extends AcceptanceTest {
 		return new RequestPostDto(title, content);
 	}
 
-	@DisplayName("게시글 목록을 조회한다")
-	@Test
-	void searchPosts() {
-		// given, when  게시글을 3개 등록한다
-		게시글_등록_되어있음("새로운 게시글 제목 - 1", "새로운 게시글 내용을 등록합니다. - 1");
-		게시글_등록_되어있음("새로운 게시글 제목 - 2", "새로운 게시글 내용을 등록합니다. - 2");
-		게시글_등록_되어있음("새로운 게시글 제목 - 3", "새로운 게시글 내용을 등록합니다. - 3");
-
-		// then  게시글 목록을 조회하여 3개가 맞는지 확인한다
-		게시글_목록_조회(3);
-	}
-
-	private void 게시글_목록_조회(int size) {
-		final List<ResponsePostDto> responsePosts = client.get().uri("/posts")
-			.accept(APPLICATION_JSON)
-			.exchange()
-			.expectStatus().isOk()
-			.expectBodyList(ResponsePostDto.class)
-			.consumeWith(getDocument("post-list-item", responseFields(
-				fieldWithPath("[]").description("array"),
-				fieldWithPath("[].id").type(JsonFieldType.STRING).description("게시글 id"),
-				fieldWithPath("[].title").type(JsonFieldType.STRING).description("게시글 제목"),
-				fieldWithPath("[].content").type(JsonFieldType.STRING).description("게시글 내용"),
-				fieldWithPath("[].createdDate").type(JsonFieldType.STRING).description("생성시간"),
-				fieldWithPath("[].modifiedDate").type(JsonFieldType.STRING).description("수정시간")
-			)))
-			.returnResult()
-			.getResponseBody();
-
-		assertThat(responsePosts).isNotNull();
-		assertThat(responsePosts.size()).isEqualTo(size);
-	}
-
-	@DisplayName("게시글을 삭제한다")
-	@Test
-	void deletePost() {
-		// TODO : 2021/06/23 게시글을 등록한다   -ksc
-		// TODO : 2021/06/23 등록된 게시글을 삭제한다   -ksc
-		// TODO : 2021/06/23 삭제되었는지 확인한다   -ksc
-	}
-
-	@DisplayName("게시글을 수정한다")
-	@Test
-	void updatePost() {
-		// TODO : 2021/06/23 게시글을 등록한다   -ksc
-		// TODO : 2021/06/23 등록된 게시글을 수정한다   -ksc
-		// TODO : 2021/06/23 수정되었는지 확인한다   -ksc
+	private ResponseFieldsSnippet getPostListResponseSnippet() {
+		return responseFields(
+			fieldWithPath("[]").description("array"),
+			fieldWithPath("[].id").type(JsonFieldType.STRING).description("게시글 id"),
+			fieldWithPath("[].title").type(JsonFieldType.STRING).description("게시글 제목"),
+			fieldWithPath("[].content").type(JsonFieldType.STRING).description("게시글 내용"),
+			fieldWithPath("[].createdDate").type(JsonFieldType.STRING).description("생성시간"),
+			fieldWithPath("[].modifiedDate").type(JsonFieldType.STRING).description("수정시간")
+		);
 	}
 }
