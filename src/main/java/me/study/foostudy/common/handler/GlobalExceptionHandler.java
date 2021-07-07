f422a1244a40c01ec14960d13d8088a3fdfbe6a6
SC <pch838811@gmail.com>
2021-07-07 22:49:09 +0900

    feat : 파라미터 밸리데이션 예외 핸들러 추가
    - 추가로 밸리데이션 예외에 대해 메시지를 테스트에서 검증하는 부분 확인이 필요함

--- src/main/java/me/study/foostudy/common/handler/GlobalExceptionHandler.java
+++ src/main/java/me/study/foostudy/common/handler/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package me.study.foostudy.common.handler;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,5 +15,16 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(IllegalArgumentException.class)
 	protected ResponseEntity<?> handleIllegalArgumentException(final IllegalArgumentException e) {
 		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+
+	@ExceptionHandler(WebExchangeBindException.class)
+	public ResponseEntity<?> handleMethodArgumentNotValidException(final WebExchangeBindException e) {
+		String messages = e.getFieldErrors()
+			.stream()
+			.map(FieldError::getDefaultMessage)
+			.filter(Objects::nonNull)
+			.map(Object::toString)
+			.collect(Collectors.joining(", "));
+		return ResponseEntity.badRequest().body(messages);
 	}
 }
