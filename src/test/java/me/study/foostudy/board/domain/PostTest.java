236ba0763baa0e0319f1e1fb6b779f5b6f401739
SC <pch838811@gmail.com>
2021-09-02 01:52:40 +0900

    test : 자신의 글이 아니면 예외가 발생하는지 entity 테스트 추가

--- src/test/java/me/study/foostudy/board/domain/PostTest.java
+++ src/test/java/me/study/foostudy/board/domain/PostTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import me.study.foostudy.board.exception.UpdateOtherUserPostException;
+
 class PostTest {
 
 	@DisplayName("글 등록")
@@ -29,6 +31,18 @@ class PostTest {
 		assertThat(newPost.getTitle()).isEqualTo("New Title");
 		assertThat(newPost.getContent()).isEqualTo("Update Content");
 		assertThat(newPost.getUserId()).isEqualTo("test-user");
+	}
+
+	@DisplayName("글 수정 시 자신의 글이 아니면 예외 발생")
+	@Test
+	void updatePost_otherUser_exception() {
+		Post newPost = createPost("New Title", "New Content", "test-user");
+
+		assertThatThrownBy(
+				() -> newPost.updateContent("other-user", "Update Content"))
+			.isInstanceOf(UpdateOtherUserPostException.class)
+			.hasMessage("다른 유저의 게시글을 수정할 수 없습니다");
+
 	}
 
 	@DisplayName("필수 값이 없으면 예외 발생")
